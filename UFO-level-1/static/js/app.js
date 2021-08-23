8beb13ee1f6ca1204b58ed5c31caef1d4c895cc9
cchitec <79786994+cchitec@users.noreply.github.com>
2021-08-23 16:00:17 -0600

    Update app.js
    update

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -2,3 +2,21 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+// Use D3 to select the table
+var table = d3.select("table");
+
+// Use D3 to select the table body
+var tbody = d3.select("tbody");
+
+// Print out the data from data.js
+console.log(data);
+
+// Function to create a table from an array of objects
+function createTable(rows){
+    rows.forEach(function(element) {
+        var row = tbody.append('tr');
+        Object.entries(element).forEach(function([key, value]) {
+            row.append('td').text(value);
+        });
+    });
+}
