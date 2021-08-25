6c01e6a541a58961f857aaa66676c4131d27197b
cchitec <79786994+cchitec@users.noreply.github.com>
2021-08-25 06:53:51 -0600

    Update app.js
    tweaks to work

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -11,6 +11,38 @@ var tbody = d3.select("tbody");
 // Print out the data from data.js
 console.log(data);
 
+
+data.forEach(function(Sighting) {
+    console.log(Sighting);
+  });
+  
+
+  data.forEach(function(Sighting) {
+    console.log(Sighting);
+    var row = tbody.append("tr");
+  });
+  
+  data.forEach(function(Sighting) {
+    console.log(Sighting);
+    var row = tbody.append("tr");
+  
+    Object.entries(Sighting).forEach(function([key, value]) {
+      console.log(key, value);
+    });
+  });
+  
+
+  data.forEach(function(Sighting) {
+    console.log(Sighting);
+    var row = tbody.append("tr");
+  
+    Object.entries(Sighting).forEach(function([key, value]) {
+      console.log(key, value);
+   
+      var cell = tbody.append("td");
+    });
+  });
+  
 // Function to create a table from an array of objects
 function createTable(rows){
     rows.forEach(function(element) {
