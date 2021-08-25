0b651cf1989d1c4da1de3b88746497ad36b2ff40
cchitec <79786994+cchitec@users.noreply.github.com>
2021-08-25 06:56:18 -0600

    Update app.js
    final tweaks

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -52,3 +52,30 @@ function createTable(rows){
         });
     });
 }
+var submit = d3.select("#filter-btn");
+submit.on("click", function() {
+
+d3.event.preventDefault();
+
+// Selecting the input element and getting the raw HTML node
+var inputElement = d3.select("#datetime");
+
+var inputValue = inputElement.property("value");
+
+//matching the inputted value with that in the stored data
+var filteredData = tableData.filter(data => data.datetime === inputValue);
+
+console.log(filteredData);
+
+
+//to clear the webpage output
+    tbody.selectAll('tr').remove();
+
+//inputs the filtered data into the table
+    filteredData.forEach(function(obj){
+        var rows = tbody.append("tr");
+        Object.keys(obj).forEach(function(key){
+            rows.append("td").text(obj[key]);
+        })
+    })
+});
